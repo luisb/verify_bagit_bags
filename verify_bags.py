9ec9e33704a5c348ac283326857e61007f8d3cf0
Luis Baquera <luisb@ucr.edu>
2024-10-08 16:29:25 -0700

    Use os.scandir in list comprehension for list of dirs/bags. Count each bag

--- verify_bags.py
+++ verify_bags.py
@@ -6,9 +6,11 @@ parser = argparse.ArgumentParser(description='Verify bags in a directory')
 parser.add_argument('directory', help='Directory containing bags to verify')
 args = parser.parse_args()
 
-for dir in os.listdir(args.directory):
-    bag_dir = os.path.join(args.directory, dir)
-    print(f'Verifying bag {bag_dir}')
+count = 0
+bag_list = [f.path for f in os.scandir(args.directory) if f.is_dir()]
+for bag_dir in sorted(bag_list):
+    count += 1
+    print(f'{count}. Verifying bag {bag_dir}')
     
     bag = bagit.Bag(bag_dir)
     if bag.is_valid():
