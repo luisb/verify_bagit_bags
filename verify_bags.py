95ed1b7795d90ab3b3d7dcbdd4d2d5f24d36dec7
Luis Baquera <luisb@ucr.edu>
2024-10-08 16:30:49 -0700

    Validate bag with 5 processes. Catch exceptions for informative output

--- verify_bags.py
+++ verify_bags.py
@@ -13,7 +13,14 @@ for bag_dir in sorted(bag_list):
     print(f'{count}. Verifying bag {bag_dir}')
     
     bag = bagit.Bag(bag_dir)
-    if bag.is_valid():
-        print(f'{bag.path} is valid')
+    try:
+        bag.validate(processes=5)
+    except bagit.BagValidationError as e:
+        for d in e.details:
+            if isinstance(d, bagit.ChecksumMismatch):
+                print(f'    Expected {d.path} to have {d.algorithm} checksum of {d.expected} but found {d.found}')
+    except bagit.BagError as e:
+        print(f'    Error encountered while validating {bag.path}:')
+        print(f'        {e}')
     else:
-        print(f'{bag.path} is not valid')
+        print(f'    {bag.path} is VALID.')
